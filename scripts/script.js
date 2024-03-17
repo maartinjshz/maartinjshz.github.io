7b060b4ecc1e6e8adce1667d999c100a577ba34d
Mārtiņš <martins.zemlitis@gmail.com>
2024-03-17 20:30:17 +0200

    js change

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,6 @@ document.getElementById("block1").addEventListener("click", function() {
     loadIframe("../templates/blog/fuzzy_transform.html");
 });
 
-
-
 function loadIframe(url) {
     document.getElementById("iframe").src = url;
 }
