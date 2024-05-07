5d2aa63dca4b86ca1ed1b698ca45892339dd3d3d
Mārtiņš <martins.zemlitis@gmail.com>
2024-05-07 23:06:46 +0300

    optimization 1_1

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,10 @@ document.getElementById("block1").addEventListener("click", function() {
     loadIframe("../templates/blog/fuzzy_transform.html");
 });
 
+document.getElementById("block2").addEventListener("click", function() {
+    loadIframe("../templates/blog/opimization_1.html");
+});
+
 function loadIframe(url) {
     document.getElementById("iframe").src = url;
 }
