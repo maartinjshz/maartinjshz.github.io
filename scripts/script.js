536d99cda35fff26737b6ead018aad712b7ee448
Mārtiņš <martins.zemlitis@gmail.com>
2024-05-07 23:18:55 +0300

    style and clear before load
    can switch from 1 blog post to other without needing to reload

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,26 @@
+
+
 document.getElementById("block1").addEventListener("click", function() {
+    // Clear existing iframe content
+    var iframe = document.getElementById("iframe");
+    iframe.src = "about:blank";
+
+    // Load new iframe content
     loadIframe("../templates/blog/fuzzy_transform.html");
 });
 
+
+
 document.getElementById("block2").addEventListener("click", function() {
+    // Clear existing iframe content
+    var iframe = document.getElementById("iframe");
+    iframe.src = "about:blank";
+
+    // Load new iframe content
     loadIframe("../templates/blog/opimization_1.html");
 });
 
 function loadIframe(url) {
-    document.getElementById("iframe").src = url;
+    var iframe = document.getElementById("iframe");
+    iframe.src = url;
 }
